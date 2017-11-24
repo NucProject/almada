c9ef0b5fd8b219ddd0f29a2060f4f1af84c85d74
healer_kx <17580246@qq.com>
2017-11-24 21:14:39 +0800

    Fix e data doc

--- api/app/Http/Controllers/EMDeviceController.php
+++ api/app/Http/Controllers/EMDeviceController.php
@@ -20,6 +20,15 @@ class EMDeviceController extends Controller
      * @param int $deviceId
      * @param int $n
      * @return array
+     *
+     * @cat data
+     * @title 电磁数据统计(E参数)接口
+     * @title 电磁数据统计(E参数)接口
+     * @url-param deviceId || int || 设备ID
+     * @url-param n || int || E参数, 例如95, 80
+     * @url-param timeBegin || int || 起始时间(秒时间戳)
+     * @url-param timeEnd || int || 结束时间(秒时间戳)
+     * @ret-val electric || float || 1.23
      */
     public function eData(Request $request, $deviceId, $n)
     {
